92ea738d5eb3ebe7efd009ff90f87890dcf95e8d
Mitchel Foster <mitchfoster3@gmail.com>
2017-06-29 14:26:03 -0600

    Swag read controller coded and running smoothly

--- server/server.js
+++ server/server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const config = require('./config.js');
+const checkForSession = require('./middlewares/checkForSession');
+const swagController = require('./controllers/swagController');
 
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use(session({
     secret: config.sessionSecret,
     resave: false,
     saveUninitialized: true
 }));
+
+app.use(checkForSession);
+
+app.get('/api/swag', swagController.read)
 
 const port = config.port || 3000
 app.listen(port, ()=> console.log(`Server online and listening on port ${port}.`) )
